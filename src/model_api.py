a268ca533cccda10aeaac1302c45ff9795d82a39
David Kim <jk2537@cornell.edu>
2023-05-16 16:36:14 -0400

    rnn test start

--- src/model_api.py
+++ src/model_api.py
@@ -1,9 +1,49 @@
 """This module contains the brains behind music generation."""
+import tensorflow as tf
+
+
+def create_model():
+    # Hyperparameters
+    batch_size = 64
+    label_size = 16
+    vocab_size = 88
+    seq_length = 16 * 4  # 10 measures since each quantum = sixteenth note
+    batch_input_shape = (batch_size, seq_length, vocab_size)
+    learning_rate = 0.01
+
+    model = tf.keras.Sequential()
+    # model.add(tf.keras.Input(batch_input_shape=batch_input_shape))
+    # model.add(tf.keras.layers.LSTM(vocab_size))
+    model.add(
+        tf.keras.layers.LSTM(
+            units=label_size * vocab_size,
+            batch_input_shape=batch_input_shape,
+            stateful=False,
+            return_sequences=False,
+        )
+    )
+    # model.add(tf.keras.layers.Dense(label_size * vocab_size, name="quantum"))
+    model.add(tf.keras.layers.Activation(tf.keras.activations.relu))
+
+    # Make sure the output shape is correct
+    model.add(tf.keras.layers.Reshape((label_size, vocab_size)))
+    # model.add(tf.keras.layers.Flatten())
+
+    # loss = {
+    #     "quantum": tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+    # }
+    loss = tf.keras.losses.Poisson()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, clipvalue=1.0)
+
+    model.compile(loss=loss, optimizer=optimizer)
+
+    return model
 
 
 class GimbopAPI:
-    def __init__(self):
-        pass
+    def __init__(self, checkpoint_filepath="./training_checkpoints/ckpt_7"):
+        self.model = create_model()
+        self.retrieve_model(checkpoint_filepath)
 
-    def retrieve_model(self):
-        return None
+    def retrieve_model(self, checkpoint_filepath="./training_checkpoints/ckpt_7"):
+        self.model.load_weights(checkpoint_filepath)
---
+++ src/models/rnn_test.py
@@ -0,0 +1,163 @@
+import os
+import glob
+import pathlib
+import logging
+import numpy as np
+import tensorflow as tf
+
+# Set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s: %(message)s", "%H:%M:%S")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# Verify that the GPU is available
+logger.info(f"GPU list: {tf.config.list_physical_devices('GPU')}")
+
+# Get path of dataset
+path = os.path.join(os.getcwd(), "data", "output")
+data_dir = pathlib.Path(path)
+logger.info(f"Looking in {data_dir}")
+
+filenames = glob.glob(str(data_dir / "*.npy"))
+logger.info(f"Number of files: {len(filenames)}")
+
+# For testing, we can use 101~150
+start_file_idx = 100
+end_file_idx = 150
+all_notes = []
+for f in filenames[start_file_idx:end_file_idx]:
+    notes = np.load(f)
+    # logger.info(notes.shape)
+    all_notes.append(notes)
+
+# Concatenate all the notes into a single numpy array with shape (num_notes, 88)
+all_notes = np.concatenate(all_notes, axis=0)
+n_notes = all_notes.shape[0]
+logger.info(f"Shape of all_notes: {all_notes.shape}")
+
+
+# Convert numpy data into tensorflow dataset
+notes_ds = tf.data.Dataset.from_tensor_slices(all_notes)
+logger.info(f"Notes element spec: {notes_ds.element_spec}")
+
+
+# Define a helper function to generate RNN input sequences
+def create_sequences(
+    dataset: tf.data.Dataset,
+    seq_length: int,
+    shift_size=4,
+    label_size=16,
+) -> tf.data.Dataset:
+    """Returns TF Dataset of sequence and label examples."""
+
+    # Take extra "label_size" length for the labels
+    seq_length = seq_length + label_size
+    windows = dataset.window(
+        seq_length, shift=shift_size, stride=1, drop_remainder=True
+    )
+
+    # `flat_map` flattens the" dataset of datasets" into a dataset of tensors
+    flatten = lambda x: x.batch(seq_length, drop_remainder=True)
+    sequences = windows.flat_map(flatten)
+
+    # Split the labels
+    def split_labels(sequences):
+        inputs = sequences[:-label_size]
+        labels = sequences[-label_size:]
+
+        return inputs, labels
+
+    return sequences.map(split_labels, num_parallel_calls=tf.data.AUTOTUNE)
+
+
+# Parameters for training the RNN
+vocab_size = 88
+seq_length = 16 * 4  # 10 measures since each quantum = sixteenth note
+shift_size = 4
+label_size = 16
+
+# Generate RNN sequences
+seq_ds = create_sequences(notes_ds, seq_length, shift_size, label_size)
+logger.info(f"Sequence element spec: {seq_ds.element_spec}")
+
+for seq, target in seq_ds.take(1):
+    logger.info(f"Sequence shape: {seq.shape}")
+    logger.info(f"Sequence elements (first 10): {seq[0:10]}")
+    logger.info("")
+    logger.info(f"Target: {target}")
+
+# Buffer size is the number of items in the dataset; e.g.:
+# 64 notes & seq = 4 & stride = 1 => buffer = 60
+# 64 notes & seq = 4 & stride = 4 => buffer = 15
+batch_size = 64
+buffer_size = (n_notes - seq_length) // shift_size
+test_ds = (
+    seq_ds.shuffle(buffer_size)
+    .batch(batch_size, drop_remainder=True)
+    .cache()
+    .prefetch(tf.data.experimental.AUTOTUNE)
+)
+logger.info(test_ds.element_spec)
+
+# Train the model
+input_shape = (seq_length, vocab_size)
+batch_input_shape = (batch_size, seq_length, vocab_size)
+learning_rate = 0.01
+
+# inputs = tf.keras.Input(input_shape)
+# x = tf.keras.layers.LSTM(128)(inputs)
+
+# outputs = {
+#     "quantum": tf.keras.layers.Dense(label_size * vocab_size, name="quantum")(x),
+# }
+
+# model = tf.keras.Model(inputs, outputs)
+
+logger.info("===== MODEL SUMMARY =====")
+logger.info(f"batch_size: {batch_size}")
+logger.info(f"seq_length: {seq_length}")
+logger.info(f"vocab_size: {vocab_size}")
+logger.info(f"input_shape: {input_shape}")
+logger.info(f"batch_input_shape: {batch_input_shape}")
+logger.info(f"learning_rate: {learning_rate}")
+logger.info("===== MODEL SUMMARY =====")
+
+model = tf.keras.Sequential()
+# model.add(tf.keras.Input(batch_input_shape=batch_input_shape))
+# model.add(tf.keras.layers.LSTM(vocab_size))
+model.add(
+    tf.keras.layers.LSTM(
+        units=label_size * vocab_size,
+        batch_input_shape=batch_input_shape,
+        stateful=False,
+        return_sequences=False,
+    )
+)
+# model.add(tf.keras.layers.Dense(label_size * vocab_size, name="quantum"))
+model.add(tf.keras.layers.Activation(tf.keras.activations.relu))
+
+# Make sure the output shape is correct
+model.add(tf.keras.layers.Reshape((label_size, vocab_size)))
+# model.add(tf.keras.layers.Flatten())
+
+# loss = {
+#     "quantum": tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+# }
+loss = tf.keras.losses.Poisson()
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, clipvalue=1.0)
+
+model.compile(loss=loss, optimizer=optimizer)
+
+# Load the model weights
+checkpoint_filepath = "./training_checkpoints/ckpt_7"
+model.load_weights(checkpoint_filepath)
+
+# Evaluate losses
+losses = model.evaluate(test_ds, return_dict=True)
+logger.info(f"Losses: {losses}")
+
+# Test the model
